35adfeaa2a1d9bf24100973466b84ad4303147f4
Taran Lunden Fjellheim <taranlf@uia.no>
2024-01-16 15:03:36 +0100

    Skrev en vits med forskjellige farger i app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+
+console.log(`${chalk.red('Hvorfor')} ${chalk.green('gikk')} ${chalk.blue('datamaskinen')} ${chalk.yellow('til')} ${chalk.green('legen')} ${chalk.magenta('?')}`);
+
+console.log(chalk.cyan(' '));
+
+console.log(`${chalk.white('Fordi')} ${chalk.green('den')}; ${chalk.blackBright('hadde')} ${chalk.blueBright('virus')} ${chalk.red('!')} ${chalk.red(' ')}`);
+
+console.log(chalk.cyan(' '));
+
+console.log(chalk.cyanBright('HaHa...')); 
+
